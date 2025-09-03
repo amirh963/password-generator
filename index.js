4e856ea7065bca9a5c9879b949272ebcc345a275
amirh963 <ah.ahmadpour55@gmail.com>
2025-09-02 21:13:23 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let passwordEl = document.getElementById("password-el")
+let passwordGl = document.getElementById("password-Gn")
+
+let characters = [
+    "A","B","C","D","E","F","G","H","I","J","K","L","M",
+    "N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+    "a","b","c","d","e","f","g","h","i","j","k","l","m",
+    "n","o","p","q","r","s","t","u","v","w","x","y","z",
+    "0","1","2","3","4","5","6","7","8","9",
+    "~","`","!","@","#","$","%","^","&","*",
+    "(",")","_","-","+","=","{","[","}","]",
+    ",","|",":",";","<",">",".","?","/"
+]
+
+passwordGl.addEventListener("click", function() {
+    let password = ""
+    for (let i = 0; i < 12; i++){
+        let ranChar = Math.floor (Math.random() * characters.length);
+        password += characters[ranChar]
+    }
+    passwordEl.value = password
+})
